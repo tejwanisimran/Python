cc71b398044b33a5d10fdc31e61fdc62601fc3b7
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-03-18 19:14:08 +0530

    Add files via upload

---
+++ Day_2/Function.py
@@ -0,0 +1,8 @@
+def Addition(No1,No2):
+    Ans = None
+    Ans = No1 + No2
+    return Ans
+
+Result = Addition(10,11)
+
+print("Addition is : ",Result)
---
+++ Day_2/Function_1.py
@@ -0,0 +1,6 @@
+No1 = 10
+No2 = 11
+
+Ans = No1 * No2
+
+print("Multiplication is : ",Ans)
