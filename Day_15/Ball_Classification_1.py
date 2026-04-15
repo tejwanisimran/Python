d2bb36b030a9f96915321b63f24e867717943718
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-04-15 14:22:10 +0530

    Add files via upload

---
+++ Day_15/Ball_Classification_1.py
@@ -0,0 +1,33 @@
+######################################################
+#
+# Steps for Machine Learning Application : 
+# 
+# Step 1 : Data Gathering / Data collection.
+# Step 2 : Data Anaylsis.
+# Step 3 : Data Cleaning.
+# Step 4 : Model Selection.
+# Step 5 : Model Training.
+# Step 6 : Model testing / evaluation.
+# Step 7 : Model improvement
+# Step 8 : Prediction / Deployment.
+#
+######################################################
+
+import sklearn
+
+def main():
+    print("Ball classification case study")
+
+    # Data Loading / Gathering is done
+
+    Features = [[35,"Rough"],[47,"Rough"],[90,"Smooth"],[48,"Rough"],[90,"Smooth"],[35,"Rough"],[92,"Smooth"],[35,"Rough"],[35,"Rough"],[35,"Rough"],[96,"Smooth"],[43,"Rough"],[110,"Smooth"],[35,"Rough"],[95,"Smooth"]]
+
+    Labels = ["Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket","Tennis","Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket"]
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+# Data set size : 15
