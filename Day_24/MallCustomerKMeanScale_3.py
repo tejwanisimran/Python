b39f7a4401103a945b0ee071bfa2ba6b1059387b
Simran Tejwani <tejwanisimran1802@gmail.com>
2026-04-27 17:38:00 +0530

    Add files via upload

---
+++ Day_24/MallCustomerKMeanScale_3.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+#-----------------------------------------------------------------------
+# Function Name : main
+# Description   : Starting point of the application
+# Parameters    : None
+# Return        : None
+# Date          : 14/03/2026
+# Author        : Simran Naveen Tejwani
+#-----------------------------------------------------------------------
+
+def main():
+    border = "="*70
+    #---------------------------------------------------------------
+    # Step 1 : Load the dataset
+    #---------------------------------------------------------------
+    print(border)
+    print("Step 1 : Load the dataset")
+    print(border)
+    df = pd.read_csv("Mall_Customers.csv")
+
+    print("First few records : ")
+    print(df.head())
+
+    print("Shape of the dataset : ")
+    print(df.shape)
+
+    print("Missing values in the dataset : ")
+    print(df.isnull().sum())
+
+    #---------------------------------------------------------------
+    # Step 2 : Select Features(Independent)
+    #---------------------------------------------------------------
+    
+    print(border)
+    print("Step 2 : Select Features(Independent)")
+    print(border)
+
+    X = df[["AnnualIncome" , "SpendingScore"]]
+
+    print("Selected fetaures : ")
+    print(X.head())
+
+    print("Shape of selected features : ")
+    print(X.shape)
+
+    #---------------------------------------------------------------
+    # Step 3 : Scale the data
+    #---------------------------------------------------------------
+    
+    print(border)
+    print("Step 3 : Scale the data")
+    print(border)
+    scalar = StandardScaler()
+
+    X_scaled = scalar.fit_transform(X)
+
+    print('Data after scalling is : ')
+    print(X_scaled[:5])
+
+if __name__ == "__main__":
+    main()
